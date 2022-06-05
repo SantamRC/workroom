8b1f1e8d406d4e171e9d65f7ca279c31a54b499c
SantamRC <santamdev404@gmail.com>
2022-06-05 16:51:24 +0530

    Added a table

--- src/App.js
+++ src/App.js
@@ -2,12 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from "./Components/Navbar";
 import Sidebar from "./Components/Sidebar";
+import AuthorNewSheetButton from "./Components/AuthorNewSheetButton";
+import DnD from "./DraggableTable/DnDBox";
+import Drag from "./DraggableTable/DragAndDrop";
+import Table from "./DraggableTable/Table";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Sidebar />
+      {/* <AuthorNewSheetButton /> */}
+      <div style={{marginLeft: 150}}>
+        <Table />
+      </div>
     </div>
   );
 }
---
+++ src/DraggableTable/Table.jsx
@@ -0,0 +1,103 @@
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemAvatar from "@mui/material/ListItemAvatar";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import Avatar from "@mui/material/Avatar";
+import IconButton from "@mui/material/IconButton";
+import FormGroup from "@mui/material/FormGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import FolderIcon from "@mui/icons-material/Folder";
+import DeleteIcon from "@mui/icons-material/Delete";
+
+function generate(element) {
+  return [0, 1, 2].map((value) =>
+    React.cloneElement(element, {
+      key: value,
+    })
+  );
+}
+
+const Demo = styled("div")(({ theme }) => ({
+  backgroundColor: theme.palette.background.paper,
+}));
+
+function Table() {
+  const [dense, setDense] = React.useState(false);
+  const [secondary, setSecondary] = React.useState(false);
+
+  return (
+    <Box sx={{width: '85vw',marginTop: '5vh'}}>
+      <Grid container spacing={2}>
+        <Grid item xs={2} style={{ backgroundColor: "green", height: "70vh" }}>
+          <Typography sx={{ mt: 4, mb: 2 }} variant="h6" component="div">
+            Icon with text
+          </Typography>
+          <Demo>
+            <List dense={dense}>
+              {generate(
+                <ListItem>
+                  <ListItemIcon>
+                    <FolderIcon />
+                  </ListItemIcon>
+                  <ListItemText
+                    primary="Single-line item"
+                    secondary={secondary ? "Secondary text" : null}
+                  />
+                </ListItem>
+              )}
+            </List>
+          </Demo>
+        </Grid>
+        <Grid item xs={8} style={{ backgroundColor: "pink", height: "70vh" }}>
+          <Typography sx={{ mt: 4, mb: 2 }} variant="h6" component="div">
+            Icon with text
+          </Typography>
+          <Demo>
+            <List dense={dense}>
+              {generate(
+                <ListItem>
+                  <ListItemIcon>
+                    <FolderIcon />
+                  </ListItemIcon>
+                  <ListItemText
+                    primary="Single-line item"
+                    secondary={secondary ? "Secondary text" : null}
+                  />
+                </ListItem>
+              )}
+            </List>
+          </Demo>
+        </Grid>
+        <Grid item xs={2} style={{ backgroundColor: "blue", height: "70vh" }}>
+          <Typography sx={{ mt: 4, mb: 2 }} variant="h6" component="div">
+            Icon with text
+          </Typography>
+          <Demo>
+            <List dense={dense}>
+              {generate(
+                <ListItem>
+                  <ListItemIcon>
+                    <FolderIcon />
+                  </ListItemIcon>
+                  <ListItemText
+                    primary="Single-line item"
+                    secondary={secondary ? "Secondary text" : null}
+                  />
+                </ListItem>
+              )}
+            </List>
+          </Demo>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+}
+
+export default Table;
